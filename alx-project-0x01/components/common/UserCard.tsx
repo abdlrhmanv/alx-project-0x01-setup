58f0bb6291a7718915c9e7dbf14286c148cfc3e4
Abdlrhman <abdlrhmanv@icloud.com>
2025-07-21 00:35:39 +0300

    3. Implement Users Card Components

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,18 +1,27 @@
 import { UserProps } from '@/interfaces';
 import React from 'react';
 
-const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
+const UserCard: React.FC<UserProps> = ({
+  id,
+  name,
+  username,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-5 space-y-2 border hover:shadow-lg transition-all">
-      <h2 className="text-xl font-semibold text-blue-700">{user.name}</h2>
-      <p className="text-sm text-gray-600">@{user.username}</p>
-      <p className="text-sm text-gray-800">{user.email}</p>
+      <h2 className="text-xl font-semibold text-blue-700">{name}</h2>
+      <p className="text-sm text-gray-600">@{username}</p>
+      <p className="text-sm text-gray-800">{email}</p>
       <p className="text-sm text-gray-600">
-        {user.address.city}, {user.address.street}, {user.address.suite}
+        {address.city}, {address.street}, {address.suite}
       </p>
-      <p className="text-sm text-gray-600">📞 {user.phone}</p>
-      <p className="text-sm text-gray-600">🌐 {user.website}</p>
-      <p className="text-sm italic text-gray-500">🏢 {user.company.name}</p>
+      <p className="text-sm text-gray-600">📞 {phone}</p>
+      <p className="text-sm text-gray-600">🌐 {website}</p>
+      <p className="text-sm italic text-gray-500">🏢 {company.name}</p>
     </div>
   );
 };
