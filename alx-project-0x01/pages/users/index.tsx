2995feb5d34df9d58a91164bbe36bc07299d750b
Abdlrhman <abdlrhmanv@icloud.com>
2025-07-21 00:37:44 +0300

    3. Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -3,29 +3,29 @@ import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const posts = await response.json();
+  
+    return {
+      props: {
+        posts,
+      },
+    };
+  }
 
-  return {
-    props: {
-      users,
-    },
+  const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+    return (
+      <div className="flex flex-col h-screen">
+        <Header />
+        <main className="p-4">
+          <div className="grid grid-cols-3 gap-2">
+            {posts.map((user) => (
+              <UserCard key={user.id} {...user} />
+            ))}
+          </div>
+        </main>
+      </div>
+    );
   };
-}
-
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
-  return (
-    <div className="flex flex-col h-screen">
-      <Header />
-      <main className="p-4">
-        <div className="grid grid-cols-3 gap-2">
-          {users.map((user) => (
-            <UserCard key={user.id} user={user} />
-          ))}
-        </div>
-      </main>
-    </div>
-  );
-};
 
 export default Users;
